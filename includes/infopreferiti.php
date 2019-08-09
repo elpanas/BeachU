609448d969480befdbe06eab32b683b9b0f60655
LucaPanas <49339752+elpanas@users.noreply.github.com>
2019-08-09 20:16:25 +0200

    Aggiunta if per caricare il menu generale

--- includes/infopreferiti.php
+++ includes/infopreferiti.php
@@ -3,6 +3,6 @@
 $elenco = estraePreferiti($db,$username);
 $output = creaElencoPreferiti($elenco);
 $text = $output['testo'];
-$encodedMarkup = $output['inlinek'];
+$encodedMarkup = ($output['inlinek'] != null) ? $output['inlinek'] : creaMenuKeyboard();
 $data = creaMsg($chatID,$text,$encodedMarkup);	
 inviaMsg($data,$url,true);
