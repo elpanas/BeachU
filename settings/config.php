b78f81080ca5087a22cbbfcdc43beba9fe7112ea
Luca Panariello <l.panariello@campus.uniurb.it>
2019-08-11 00:44:39 +0200

    Aggiunto file env

--- settings/config.php
+++ settings/config.php
@@ -1,6 +1,7 @@
 <?php
-define('BOT_TOKEN', '936947513:AAEZUdAcEhYL8tSqF_TdEmZGH97WvwSszf8');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+// define('BOT_TOKEN', '936947513:AAEZUdAcEhYL8tSqF_TdEmZGH97WvwSszf8');
+$bot_token = getenv("BOT_TOKEN");
+define('API_URL', 'https://api.telegram.org/bot'.$bot_token.'/');
 define('MAPBOX_TOKEN', 'pk.eyJ1IjoibHVrZTE5ODMiLCJhIjoiY2p5eTd3eGZ5MWV5YTNkcnI2amJqbWFrbyJ9.jtWFPk-5ju4CcAjZI7nDUg');
 define('MAPBOX_URL', 'https://api.mapbox.com/geocoding/v5/mapbox.places/');
 define('RICERCA_URL', 'https://www.google.com/search?q=');
