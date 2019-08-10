2e43a6e2100a92eafe4cec31481d66ae201ac66a
Luca Panariello <l.panariello@campus.uniurb.it>
2019-08-11 00:59:22 +0200

    Aggiornato file index

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require 'vendor/autoload.php';
 $dotenv = new DotenvDotenv(__DIR__);
 $dotenv->load();
+echo $bot_token = getenv("BOT_TOKEN");
 require 'settings/config.php';
 require 'db.php';
 require 'functions.php';
