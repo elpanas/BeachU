3b4d85b29e43954d2b577fe61ca387dd546ebd1e
LucaPanas <49339752+elpanas@users.noreply.github.com>
2019-08-11 15:58:12 +0200

    Aggiunti argomenti mancanti

--- includes/infolocalita.php
+++ includes/infolocalita.php
@@ -2,7 +2,7 @@
 $elenco = null; // inizializza la variabile
 
 $elenco = estraeElenco($db,$messaggio); // estrae i disponibili dal db
-$output = creaElenco($elenco); // crea l'elenco degli stabilimenti
+$output = creaElenco($elenco,false); // crea l'elenco degli stabilimenti
 $text = $output['testo'];
 
 if ($output['inlinek'] != null) $encodedMarkup = $output['inlinek'];
