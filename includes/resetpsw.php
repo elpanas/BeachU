2ef5815da06c4e4f16074f422640fcb862d65630
Luca Panariello <l.panariello@campus.uniurb.it>
2019-08-11 14:43:50 +0200

    Correzioni

--- includes/resetpsw.php
+++ includes/resetpsw.php
@@ -2,5 +2,5 @@
 $text = "Continuando resetterai la password attuale";
 $text .= "I tuoi dati NON saranno cancellati";
 
-$inline_keyboard['inline_keyboard'][$i][0]['text'] = 'Premi qui per confermare?';
-$inline_keyboard['inline_keyboard'][$i][0]['callback_data'] = '/rY';
+$inline_keyboard['inline_keyboard'][0][0]['text'] = 'Premi qui per confermare?';
+$inline_keyboard['inline_keyboard'][0][0]['callback_data'] = '/rY';
