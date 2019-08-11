ccb6a7b7d69bcc0a3de06ce3b25818fbebcbbe18
Luca Panariello <l.panariello@campus.uniurb.it>
2019-08-11 14:47:49 +0200

    Prova

--- includes/resetpsw.php
+++ includes/resetpsw.php
@@ -4,3 +4,9 @@ $text .= "I tuoi dati NON saranno cancellati";
 
 $inline_keyboard['inline_keyboard'][0][0]['text'] = 'Premi qui per confermare?';
 $inline_keyboard['inline_keyboard'][0][0]['callback_data'] = '/rY';
+
+$encodedMarkup = json_encode($inline_keyboard);
+
+$data = creaMsg($chatID,$text,$encodedMarkup); // crea il messaggio
+
+inviaMsg($data,$url,true); // invia il messaggio
