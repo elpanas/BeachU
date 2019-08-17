9419621c1bd5c7a8b77f0e73795134edfe9f4c4e
Luca Panariello <l.panariello@campus.uniurb.it>
2019-08-17 16:20:06 +0200

    Corretto nome funzione

--- includes/stabilimento.php
+++ includes/stabilimento.php
@@ -2,7 +2,7 @@
 inviaMsg($data,$url,true); // invia la risposta al callback
 
 $dati_stab = estraeDisp($db,$id_stabilimento); // estrae i dati dello stabilimento
-$coordinate = forwardMapbox($dati_stab['indirizzo']); // codifica l'indirizzo per essere inserito in un url
+$coordinate = mapboxForward($dati_stab['indirizzo']); // codifica l'indirizzo per essere inserito in un url
 
 // controlla se è già presente tra i preferiti e se l'utente è loggato
 if (!controllaPreferito($db,$id_stabilimento,$username))
